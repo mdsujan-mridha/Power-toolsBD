6522c17b5eac07472ff0e159d6c20fa698918ae6
mdsujon-mridha <mdsujan134523@gmail.com>
2022-05-27 02:18:07 +0600

    connect with firebase

--- src/Pages/Authentication/Login.js
+++ src/Pages/Authentication/Login.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 import SocialLogin from './SocialLogin';
 
 
 const Login = () => {
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+
+    const emailRef = useRef();
+    const passworRef = useRef();
+
+    const handleLogin = e => {
+
+        const email = emailRef.current.value;
+        const password = passworRef.current.value;
+        signInWithEmailAndPassword(email,password);
+
+
+    }
+
     return (
         <div className='flex justify-center items-center h-screen'>
             <div class="card w-96 bg-base-100 shadow-xl">
                 <div class="card-body items-center">
                     <h2 class=" text-center font-bold text-4xl"> Login  </h2>
                     <div className='mt-10'>
-                        <form>
-                            <input className='border-2 w-full rounded-md h-12 mt-5' type="email" name="email" id="1" />
-                            <input className='border-2 w-full rounded-md h-12 mt-5' type="password" name="password" id="2" />
+                        <form onSubmit={handleLogin}>
+                            <input ref={emailRef} className='border-2 w-full rounded-md h-12 mt-5' placeholder='email' type="email" name="email" id="1" required />
+                            <input ref={passworRef} className='border-2 w-full rounded-md h-12 mt-5' placeholder='password' type="password" name="password" id="2" required />
                             <button className='mt-5 text-primary font-semibold text-md'> Forgot Password? </button>
                             <input type="submit" className='btn btn-primary text-white w-full max-w-full text-white-100 font-medium mt-9 border-2' value="Login" />
                             <p className='my-5 font-semibold text-md'>New in Power ToolsBD? <button className='text-primary ml-3'> <Link to="/register"> Create an Account </Link> </button> </p>
--- src/Pages/Authentication/Register.js
+++ src/Pages/Authentication/Register.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 import SocialLogin from './SocialLogin';
 
 const Register = () => {
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+
+
+const nameRef = useRef();
+const emailRef = useRef();
+const passwordRef = useRef();
+
+      const handleSignUp = e =>{
+          e.preventDefault()
+          const name = nameRef.current.value;
+          const email = emailRef.current.value;
+          const passsword = passwordRef.current.value;
+          createUserWithEmailAndPassword(email,passsword)
+
+
+      }
     
 
     return (
@@ -11,11 +35,11 @@ const Register = () => {
                 <div class="card-body">
                     <h2 class="text-center text-4xl my-5 font-bold "> Signup </h2>
                     <div>
-                        <form>
+                        <form onSubmit={handleSignUp}>
 
-                            <input className='border-2 w-full rounded-md h-12 mt-5' placeholder='Name' type="text" name="name" id="1" />
-                            <input className='border-2 w-full rounded-md h-12 mt-5' placeholder='E-mail' type="email" name="email" id="2" />
-                            <input className='border-2 w-full rounded-md h-12 mt-5' placeholder='password' type="password" name="password" id="3" />
+                            <input ref={nameRef} className='border-2 w-full rounded-md h-12 mt-5' placeholder='Name' type="text" name="name" id="1" required/>
+                            <input  ref={emailRef}className='border-2 w-full rounded-md h-12 mt-5' placeholder='E-mail' type="email" name="email" id="2" required/>
+                            <input ref={passwordRef} className='border-2 w-full rounded-md h-12 mt-5' placeholder='password' type="password" name="password" id="3" required/>
                             <input className='btn btn-primary text-white w-full max-w-full text-white-100 font-medium mt-9 border-2' type="submit" value="Signup" />
                             <p className='my-5 text-lg font-semibold'>Already have an account? <Link className='text-primary' to="/login">Login </Link>  </p>
                         </form>
